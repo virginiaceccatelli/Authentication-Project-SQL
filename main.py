da340c9480e3caa02bc8e6ed8c05eebdc69b55dc
virginiaceccatelli <nina.ceccatelli@gmail.com>
2024-04-17 15:06:19 +0200

    updated authentication project

--- main.py
+++ main.py
@@ -24,8 +24,13 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
 # CREATED TABLE IN DB
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
@@ -38,7 +43,7 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["POST", "GET"])
@@ -48,41 +53,48 @@ def register():
         unhashed_p = request.form.get("password")
         password_ = werkzeug.security.generate_password_hash(unhashed_p, method='pbkdf2:sha256', salt_length=8)
         name_ = request.form.get("name")
-        registered_user = User(email=email_, password=password_, name=name_)
-        db.session.add(registered_user)
-        db.session.commit()
-        print("committed")
+        existing_user = User.query.filter_by(email=email_).first()
+        if existing_user:
+            flash("You are already registered, log in instead.")
+        else:
+            registered_user = User(email=email_, password=password_, name=name_)
+            db.session.add(registered_user)
+            db.session.commit()
         return render_template("secrets.html", name=request.form.get('name'))
-    return render_template("register.html")
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+    return render_template("register.html", logged_in = current_user.is_authenticated, name=request.form.get('name'))
 
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    form = User()
-    if form.validate_on_submit():
-        login_user(User.user)
-        flash("Logged in successfully")
-        return redirect(url_for("secrets", name=form.name))
-    return render_template("login.html", form=form)
+    if request.method == "POST":
+        email_ = request.form.get("email")
+        unhashed_p = request.form.get("password")
+        user = User.query.filter_by(email=email_).first()
+        check_p = werkzeug.security.check_password_hash(user.password, unhashed_p)
+        if not user or not check_p:
+            flash("Invalid credentials, please try again.")
+        if user and check_p:
+            login_user(user)
+            flash("Logged in successfully")
+            return redirect(url_for("secrets", name=User.name))
+    return render_template("login.html", logged_in = current_user.is_authenticated)
 
 
 @app.route('/secrets', methods=["POST", "GET"])
+@login_required
 def secrets():
     all_users = User.query.all()
-    return render_template("secrets.html", users=all_users)
+    return render_template("secrets.html", users=all_users, name=current_user.name)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route('/download', methods=["GET"])
+@login_required
 def download():
     return send_from_directory(directory="static", path="files/cheat_sheet.pdf")
 
